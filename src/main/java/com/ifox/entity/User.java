52b5e99e32fbaeaf143624894fcfecbb3666c9f4
danchede <1320030382@qq.com>
2017-12-06 21:53:25 +0800

    创建好实体类

---
+++ src/main/java/com/ifox/entity/User.java
@@ -0,0 +1,67 @@
+package com.ifox.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private int age;
+    private String address;
+
+    public User() {
+    }
+
+    public User(String firstname, String lastname, int age, String address) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.age = age;
+        this.address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
